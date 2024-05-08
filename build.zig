870ef39a40bb079002be571a618849d0c60d0413
lukesnc <lukeasimone@gmail.com>
2024-05-07 22:09:58 -0400

    fix zig build

--- build.zig
+++ build.zig
@@ -4,14 +4,28 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "bf-jit",
+        .name = "bf",
         .target = target,
         .optimize = optimize,
     });
     exe.linkLibC();
-    exe.addCSourceFiles(&.{
-        "src/main.c",
-    }, &.{});
+    exe.addCSourceFiles(.{
+        .files = &.{
+            "src/main.c",
+        },
+    });
 
     b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
